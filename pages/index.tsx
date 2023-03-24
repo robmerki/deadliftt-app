a29d75924a66741b5b284d49fa13f5f9b0fccea7
Robert Merki <robmerki@gmail.com>
2023-03-24 13:41:52 -0400

    fixed prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='w-full h-screen bg-slate-600'>
-        
-      </main>
+      <main className="w-full h-screen bg-slate-600"></main>
     </>
   )
 }
