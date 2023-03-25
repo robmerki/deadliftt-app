ff1ff429939420eaaecbbf490d045062a248063e
Robert Merki <robmerki@gmail.com>
2023-03-25 16:10:09 -0400

    update prompt

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -25,31 +25,31 @@ export default async function handler(
 }
 
 function createPrompt(category: string, keywords: string): string {
-  return `I am a YouTube strategist that his planning content for a client who is interested in making content about: ${category}
-  I'd like for you to do a mind map of every category that the following keywords could touch: ${keywords}.
+  return `I am a content strategist that his planning content for a client who is interested in making content about: ${category}
+  I'd like for you to create a mind map of interesting content that could be related to ${category} and following keywords: ${keywords}.
   
-  Please format the results as a nested JSON object with where the keys are the keywords and the values are the categories that the keyword could touch.
+  Please format the results as a nested JSON object with where the keys are the keywords and the values are the potential video titles.
   
   The results should have the following structure: 
   {
     "keyword1": {
-      "category1": [
-        "bullet1",
-        "bullet2"
+      "subkeyword1": [
+        "idea1",
+        "idea2"
       ],
-      "subcategory1": [
-        "bullet1",
-        "bullet2"
+      "subkeyword2": [
+        "idea1",
+        "idea2"
       ]
     },
     "keyword2": {
-      "subcategory2": [
-        "bullet1",
-        "bullet2"
+      "subkeyword1": [
+        "idea1",
+        "idea2"
       ],
-      "subcategory3": [
-        "bullet1",
-        "bullet2"
+      "subkeyword2": [
+        "idea1",
+        "idea2"
       ]
     }
   }
